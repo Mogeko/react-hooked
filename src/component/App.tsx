d6ca9cef58d4bed181e2f5434308868325412d87
Mogeko <zhengjunyi@live.com>
2020-10-27 22:02:57 +0100

    feat(component): Realize the complete logic

--- src/component/App.tsx
+++ src/component/App.tsx
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from '../logo.svg';
+import React, { useEffect, useState } from 'react';
 import '../App.css';
+import Header from './Header';
+import Movie, { MovieData } from './Movie';
+import Search from './Search';
 
-function App() {
+const MOVIE_API_URL = "https://www.omdbapi.com/?s=man&apikey=4a3b711b"
+
+export default function App() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  useEffect(() => {
+    fetch(MOVIE_API_URL)
+      .then(response => response.json())
+      .then(jsonResponse => {
+        setMovies(jsonResponse.Search);
+        setLoading(false);
+      });
+  }, []);
+
+  const search = (searchData: string) => {
+    setLoading(true);
+    setErrorMessage(null);
+
+    fetch(`https://www.omdbapi.com/?s=${searchData}&apikey=4a3b711b`)
+      .then(response => response.json())
+      .then(jsonResponse => {
+        jsonResponse.Response === 'True' ? 
+        setMovies(jsonResponse.Search) : 
+        setErrorMessage(jsonResponse.Error);
+        setLoading(false);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header text="HOOKED" />
+      <Search search={search} />
+      <p className="App-intro">Sharing a few of our favourite movies</p>
+      <div className="movies">
+        {loading && !errorMessage ? (
+         <span>loading...</span>
+         ) : errorMessage ? (
+          <div className="errorMessage">{errorMessage}</div>
+        ) : (
+          movies.map((movie: MovieData, index) => (
+            <Movie key={`${index}-${movie.Title}`} movie={movie} />
+          ))
+        )}
+      </div>
     </div>
   );
-}
-
-export default App;
+};
