71cdafaa139623d505c2c7796e74c6a2cad80062
Mogeko <zhengjunyi@live.com>
2020-10-28 01:04:31 +0100

    test(App): Add unit test for App.tsx

---
+++ src/App.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './component/App';
+
+test('Title', () => {
+  render(<App />);
+  const linkElement = screen.getByText("HOOKED");
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('Hint', () => {
+  render(<App />);
+  const linkElement = screen.getByText("Sharing a few of our favourite movies");
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('Loading', () => {
+  render(<App />);
+  const linkElement = screen.getByText("loading...");
+  expect(linkElement).toBeInTheDocument();
+});
